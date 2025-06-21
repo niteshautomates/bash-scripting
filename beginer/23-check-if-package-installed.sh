e64eb14b434dfb74e79f1c63073984893ce24330
Nitesh Wayafalkar <niteshwayafalkar@yahoo.com>
2025-06-21 19:23:52 +0530

    feat: fixed 23 script

--- beginer/23-check-if-package-installed.sh
+++ beginer/23-check-if-package-installed.sh
@@ -7,9 +7,10 @@ if dpkg -s "$pkg" &> /dev/null;then
 	echo " $pkg is present"
 else
 	echo "$pkg is not installed"
-	echo "Do you wnat to install $pkg (yes/no)" choice
-	if [ $choice == "yes" ];then
-		if [ $pkg == "docker" ];then
+	echo "Do you want to install $pkg (yes/no)"
+	read choice
+	if [ "$choice" = "yes" ];then
+		if [ $pkg = "docker" ];then
 			echo "$(apt-get install docker.io -y)"
 		else
 			echo "$( apt-get install $pkg -y)"
